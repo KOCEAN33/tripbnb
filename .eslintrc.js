0831cd58c5c6022658d013ae42c194356899a1fa
KOCEAN <dev@xanny.us>
2023-04-06 05:56:30 +0900

    add modal, button

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'next',
+    'prettier',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*js'],
+      parser: '@typescript-eslint/parser',
+    },
+  ],
+};
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,35 @@
-import {Nunito} from "next/font/google";
+import { Nunito } from 'next/font/google';
 
-import './globals.css'
+import './globals.css';
+
+import ClientOnly from '@/app/components/ClientOnly';
+import Navbar from '@/app/components/navbar/Navbar';
+import Modal from '@/app/components/Modals/Modal';
 
 export const metadata = {
   title: 'Airbnb',
-  description: 'Airbnb Clone'
-}
+  description: 'Airbnb Clone',
+};
 
 const font = Nunito({
-  subsets: ["latin"]
-})
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  // @ts-ignore
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <ClientOnly>
+          <Modal actionLabel="Submit" title="Hello World" isOpen />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
