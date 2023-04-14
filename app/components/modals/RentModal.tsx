4037d7ac4914a502befc8e5ca3e961ebb522aaa9
KOCEAN <dev@xanny.us>
2023-04-15 04:14:52 +0900

    add rentmodal

---
+++ app/components/modals/RentModal.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Modal from './Modal';
+import useRentModal from '@/app/hooks/useRentModal';
+
+const RentModal = () => {
+  const rentModal = useRentModal();
+
+  return <div></div>;
+};
+
+export default RentModal;
