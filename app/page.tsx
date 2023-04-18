059f9bb6212730dee221323c610e68dcafb16959
KOCEAN <dev@xanny.us>
2023-04-19 06:15:18 +0900

    add main page ListingCard.tsx
    Signed-off-by: KOCEAN <dev@kocean.me>

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,47 @@
-export default function Home() {
-  return <div className="text-2xl text-rose-500">Hello Airbnb!</div>;
+import Container from '@/app/components/Container';
+import ClientOnly from './components/ClientOnly';
+import EmptyState from '@/app/components/EmptyState';
+import getListings from '@/app/actions/getListings';
+import ListingCard from '@/app/components/listings/ListingCard';
+import getCurrentUser from '@/app/actions/getCurrentUser';
+
+export default async function Home({}) {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+          className="
+            grid
+            grid-cols-1
+            gap-8
+            pt-24
+            sm:grid-cols-2
+            md:grid-cols-3
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6"
+        >
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                currentUser={currentUser}
+                key={listing.id}
+                data={listing}
+              />
+            );
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
+  );
 }
